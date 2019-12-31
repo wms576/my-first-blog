7e2e1da88cd61220170d7c28ca345d99d56f1513
wms576 <597130713@qq.com>
2019-12-31 16:53:30 +0800

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'xjq',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
