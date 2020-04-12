8f49fa0ae5a15b8ca678c4be625bcdd94e470877
wms576 <597130713@qq.com>
2020-04-12 14:25:47 +0800

    try2

--- ddj/views.py
+++ ddj/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Ddjw
+import random
 
 def ddj_list(request):
-    return render(request,'ddj/ddj_list.html',{})
+    ddjws=Ddjw.objects.filter(zjh=random.randint(1,81))
+    return render(request,'ddj/ddj_list.html',{'ddjws':ddjws})
